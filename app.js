79b5476adf87cfde8e6d3ee89377b977899b0e76
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-09-09 01:33:08 +0500

    process

--- app.js
+++ app.js
@@ -27,33 +27,77 @@ console.log(users['login form [2]']);
 
 
 
-const gymmembership={
-      name: 'Bluk',
-      age:17,
-      fees: 2500,
-      discount: 10,
-      greetuser:function (members) {
-            let discountuser;
-            if (members >= 2) {
-                  discountuser = 20
-                  console.log(discountuser)
-                  this.fees= this.fees* discountuser / 100
+// const gymmembership={
+//       name: 'Bluk',
+//       age:17,
+//       fees: 2500,
+//       discount: 10,
+//       greetuser:function (members) {
+//             let discountuser;
+//             if (members >= 2) {
+//                   discountuser = 20
+//                   console.log(discountuser)
+//                   this.fees= this.fees* discountuser / 100
                   
-            }
-            else{
-             discountuser = 5
-             console.log(discountuser)
-             this.fees= this.fees* discountuser/ 100
+//             }
+//             else{
+//              discountuser = 5
+//              console.log(discountuser)
+//              this.fees= this.fees* discountuser/ 100
 
-            }
+//             }
             
-      }
-}
+//       }
+// }
 
-console.log(gymmembership.discount(3));
-
-
+// console.log(gymmembership.discount(3));
 
 
+window.alert("Please enter the input")
 
 
+
+const numbers= 55
+
+console.log(numbers*4);
+
+const orignalnums = 90
+// orignalnums = orignalnums + 10
+
+ var originalNum = 90;
+ originalNum = originalNum + 10;
+
+
+// console.log(originalNum);
+
+var prezWhoCame1st = "Washington";
+console.log(prezWhoCame1st);
+
+
+const csbxud= 3443
+alert(csbxud)
+
+
+var popularNumber = 12 - 24;
+console.log(popularNumber);
+
+
+var whatsLeftOver = 8 % 3;
+console.log(whatsLeftOver);
+
+
+var num = 1;
+var newNum = num++;
+
+console.log(newNum);
+
+
+ var num = 1;
+ var newNum = ++num;
+
+ console.log(newNum);
+
+ var num = 1;
+ var newNum = num--;
+
+ console.log(num);
