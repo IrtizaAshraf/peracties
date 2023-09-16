e48ffb9f0a9676f951a438f8b054432815ac700f
Irtiza Ashraf <irtizaashraf802@gmail.com>
2023-09-16 21:13:29 +0500

    done

--- app.js
+++ app.js
@@ -1,109 +1,40 @@
-const user ={
-      name: 'Muhammad Irtiza',
-      age:17,
-      login: ['Monday', 'Thuesday','wednesday']
-}
+let complexNestedObject = {
+      prop1: "value1",
+      prop2: {
+          nestedProp1: "nestedValue1",
+          nestedProp2: [1, 2, 3, 4],
+          nestedProp3: {
+              deepNestedProp1: "deepNestedValue1",
+              deepNestedProp2: [5, 6, 7, 8],
+              deepNestedProp3: {
+                  deeperNestedProp1: "deeperNestedValue1",
+                  deeperNestedProp2: [9, 10, 11, 12]
+              }
+          }
+      },
+      prop3: [
+          {
+              arrayProp1: "arrayValue1",
+              arrayProp2: [13, 14, 15]
+          },
+          {
+              arrayProp1: "arrayValue2",
+              arrayProp2: [16, 17, 18]
+          }
+      ],
+      prop4: {
+          nestedProp4: "nestedValue4",
+          nestedProp5: [
+              {
+                  deeperArrayProp1: "deeperArrayValue1",
+                  deeperArrayProp2: [19, 20, 21]
+              },
+              {
+                  deeperArrayProp1: "deeperArrayValue2",
+                  deeperArrayProp2: [22, 23, 24]
+              }
+          ]
+      }
+  };
 
-
-console.log(user.name);
-console.log(user.age);
-
-
-
-const users ={
-      'last names': 'Muhammad Irtiza',
-      age:17,
-      'login form': ['Monday', 'Thuesday','wednesday']
-}
-
-console.log(users['last names']);
-console.log(users['login form [2]']);
-
-
-
-
-
-
-
-
-
-// const gymmembership={
-//       name: 'Bluk',
-//       age:17,
-//       fees: 2500,
-//       discount: 10,
-//       greetuser:function (members) {
-//             let discountuser;
-//             if (members >= 2) {
-//                   discountuser = 20
-//                   console.log(discountuser)
-//                   this.fees= this.fees* discountuser / 100
-                  
-//             }
-//             else{
-//              discountuser = 5
-//              console.log(discountuser)
-//              this.fees= this.fees* discountuser/ 100
-
-//             }
-            
-//       }
-// }
-
-// console.log(gymmembership.discount(3));
-
-
-window.alert("Please enter the input")
-
-
-
-const numbers= 55
-
-console.log(numbers*4);
-
-const orignalnums = 90
-// orignalnums = orignalnums + 10
-
- var originalNum = 90;
- originalNum = originalNum + 10;
-
-
-// console.log(originalNum);
-
-var prezWhoCame1st = "Washington";
-console.log(prezWhoCame1st);
-
-
-const csbxud= 3443
-alert(csbxud)
-
-
-var popularNumber = 12 - 24;
-console.log(popularNumber);
-
-
-var whatsLeftOver = 8 % 3;
-console.log(whatsLeftOver);
-
-
-var num = 1;
-var newNum = num++;
-
-console.log(newNum);
-
-
- var num = 1;
- var newNum = ++num;
-
- console.log(newNum);
-
- var num = 1;
- var newNum = num--;
-
- console.log(num);
-
-
-
- for(let i=1; ;i++) {
-      const data= i*5; console.log(data)
-        if(data>=30) break; }
+  console.log(complexNestedObject.prop4.nestedProp5[1].deeperArrayProp2[2]);
